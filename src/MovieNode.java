b4a22cec3dddf5bf0e08053ad182893d6f2ca885
clsankaran <clsankaran@wpi.edu>
2017-11-11 22:26:44 -0500

    Movie Node

--- src/MovieNode.java
+++ src/MovieNode.java
@@ -1,2 +1,23 @@
-public class MovieNode {
+import java.util.*;
+public class MovieNode implements Node {
+
+    private String _name;
+    final private ArrayList<Node> _listOfActors = new ArrayList<ActorNode>();
+
+    public MovieNode (String name) {
+        _name = name;
+    }
+
+    public String getName () {
+        return _name;
+    }
+
+    public Collection<? extends Node> getNeighbors () {
+        return _listOfActors;
+    }
+
+    public void addToNeighnors (ActorNode actor) {
+        _listOfActors.add(actor);
+    }
+
 }
