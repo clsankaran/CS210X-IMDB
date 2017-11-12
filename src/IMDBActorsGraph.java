43a9a9b972e8addb69cb9d60f2e6d2c580ce7b9d
clsankaran <clsankaran@wpi.edu>
2017-11-11 21:06:20 -0500

    setup imdg actor and movie graphs

---
+++ src/IMDBActorsGraph.java
@@ -0,0 +1,20 @@
+import java.util.Collection;
+
+public class IMDBActorsGraph implements Graph {
+    /**
+     * Gets all the nodes in the graph.
+     * @return a collection of all the nodes in the graph.
+     */
+    public Collection<? extends Node> getNodes (){
+        return null;
+    }
+    /**
+     * Returns the Node having the specified name.
+     * @param name the name of the requested Node
+     * @return the node associated with the specified name or null
+     * if no such node exists.
+     */
+    public Node getNodeByName (String name){
+        return null;
+    }
+}
