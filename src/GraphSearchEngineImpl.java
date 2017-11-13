275ca4daa673afcf70d504e79616ff2ddbdaa955
clsankaran <clsankaran@wpi.edu>
2017-11-13 08:23:51 -0500

    shortest path

--- src/GraphSearchEngineImpl.java
+++ src/GraphSearchEngineImpl.java
@@ -1,6 +1,8 @@
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
+import java.util.ArrayList;
 
 public class GraphSearchEngineImpl implements GraphSearchEngine{
 /*
@@ -24,7 +26,7 @@ return visitedNodes;}
 
 
     public List<Node> findShortestPath (Node s, Node t){
-        List<Node> visitedNodes = new LinkedList<Node>();
+        /*List<Node> visitedNodes = new LinkedList<Node>();
         Queue<Node> nodesToVisit = new LinkedList<Node>();
 
         nodesToVisit.add(s);
@@ -40,8 +42,56 @@ return visitedNodes;}
                 }
             }
         }
-        return visitedNodes;
-     }
+        return visitedNodes;*/
 
+        ArrayList<Node> shortestPathList = new ArrayList<Node>();
+        ArrayList<Node> visited = new ArrayList<Node>();
+
+        if (s.equals(t))
+            return null;
+
+        Queue<Node> toVisit = new LinkedList<Node>();
+        Stack<Node> pathStack = new Stack<Node>();
+
+        toVisit.add(s);
+        pathStack.add(s);
+        visited.add(s);
+
+        while(!toVisit.isEmpty())
+        {
+            Node u = toVisit.poll();
+
+            for(Node neighbor : u.getNeighbors())
+            {
+                if(!visited.contains(neighbor))
+                {
+                    toVisit.add(neighbor);
+                    visited.add(neighbor);
+                    pathStack.add(neighbor);
+                    if(u == t)
+                        break;
+                }
+            }
+        }
+
+
+        //To find the path
+        Node node;
+        Node currentSrc = t;
+        shortestPathList.add(t);
+        while(!pathStack.isEmpty())
+        {
+            node = pathStack.pop();
+            if(currentSrc.getNeighbors().contains(node))
+            {
+                shortestPathList.add(node);
+                currentSrc = node;
+                if(node == s)
+                    break;
+            }
+        }
+
+        return shortestPathList;
+     }
 
 }
