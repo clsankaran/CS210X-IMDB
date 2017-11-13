43c5cc5cdcb85cef229a22158884186ded2c0e81
nilay <nilaybarde@gmail.com>
2017-11-12 21:01:04 -0500

    Nilay Work

--- src/GraphSearchEngineImpl.java
+++ src/GraphSearchEngineImpl.java
@@ -1,7 +1,47 @@
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class GraphSearchEngineImpl implements GraphSearchEngine{
+/*
+
+List bfs (Node s) {
+List visitedNodes;
+Queue nodesToVisit;
+
+nodesToVisit.enqueue(s);
+while (nodesToVisit.size() > 0) {
+n = nodesToVisit.dequeue();
+visitedNodes.add(n);
+for each neighbor n' of n:
+if (nodesToVisit doesn’t already contain n'
+&& we have not yet visited n'):
+nodesToVisit.enqueue(n');
+}
+return visitedNodes;}
+
+ */
+
+
     public List<Node> findShortestPath (Node s, Node t){
-        return null;
-    }
+        List<Node> visitedNodes = new LinkedList<Node>();
+        Queue<Node> nodesToVisit = new LinkedList<Node>();
+
+        nodesToVisit.add(s);
+
+        while(nodesToVisit.size() > 0) {
+            Node n = nodesToVisit.remove();
+            visitedNodes.add(n);
+
+            for(Node neigh: n.getNeighbors()){
+                if((!n.getNeighbors().contains(neigh)) || !visitedNodes.contains(neigh)){
+                    nodesToVisit.add(neigh);
+
+                }
+            }
+        }
+        return visitedNodes;
+     }
+
+
 }
